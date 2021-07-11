237fc54bf89e8bb69037ffd77db1fbd857ed97f7
TiDev00 <cisse.thierno@outlook.com>
2021-07-11 22:36:07 +0000

    last state

--- assignement1/src/App.js
+++ assignement1/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from "./components/MenuComponent";
-
+import { dishes } from "shared/dishes";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+    return (
+        <div className="App">
+            <Navbar dark color="primary">
+                <div className="container">
+                    <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu dishes={this.state.dishes}/>
         </div>
-      </Navbar>
-        <Menu>
-
-        </Menu>
-    </div>
-  );
+    );
 }
-
 export default App;
